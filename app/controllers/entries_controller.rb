0a7b92310c8270652b4e22ff08b22dca9f5799c5
Pablo Hennique <phennique@gmail.com>
2023-06-07 14:48:43 -0400

    implemented interaction with gpt to provide gratefulness statement

--- app/controllers/entries_controller.rb
+++ app/controllers/entries_controller.rb
@@ -4,26 +4,68 @@ class EntriesController < ApplicationController
   end
 
   # this method is for OPEN AI testing purposes only
-  def chat_test
-    @client = OpenAI::Client.new
-    @response = @client.chat(
-      parameters: {
-        model: "gpt-3.5-turbo",
-        messages: [{ role: "user", content: params[:query] }],
-        temperature: 0.3
-      }
-    )
-  end
+  # def chat_test
+  #   @client = OpenAI::Client.new
+  #   @response = @client.chat(
+  #     parameters: {
+  #       model: "gpt-3.5-turbo",
+  #       messages: [{ role: "user", content: params[:query] }],
+  #       temperature: 0.3
+  #     }
+  #   )
+  # end
 
   def new
     @entry = Entry.new
   end
 
   def create
-    @entry = Entry.new(content: params[:entry][:content], user: current_user, date:Date.today)
+    @entry = Entry.new(content: params[:entry][:content], user: current_user, date: Date.today)
     @entry.save
+    sentiment_analysis
+    turn_to_gratefulness if @sentiment == "Positive"
     redirect_to entries_path
+    raise
   end
 
+  private
 
+  def sentiment_analysis
+    @client = OpenAI::Client.new
+    @response = @client.chat(
+      parameters: {
+        model: "gpt-3.5-turbo",
+        messages: [{ role: "user",
+                     content: "Indicate if the following entry has a positive or non-positive sentiment.
+                              Permited responses: 'Positive' or 'Non-Positive' #{params[:entry][:content]}" }],
+        temperature: 0.3
+        # max_tokens: 30
+      }
+    )
+    @sentiment = @response["choices"][0]["message"]["content"]
+    @sentiment.chop! if @sentiment.last == "."
+  end
+
+  def turn_to_gratefulness
+    if params[:entry][:content].length < 30
+      @gratefulness = params[:entry][:content]
+    else
+      gpt_gratefulness
+      @gratefulness = @response["choices"][0]["message"]["content"]
+    end
+  end
+
+  def gpt_gratefulness
+    @client = OpenAI::Client.new
+    @response = @client.chat(
+      parameters: {
+        model: "gpt-3.5-turbo",
+        messages: [{ role: "user",
+                     content: "Write the thesis statement of the following entry in the frist person:
+                              #{params[:entry][:content]}" }],
+        temperature: 0.1,
+        max_tokens: 30
+      }
+    )
+  end
 end
