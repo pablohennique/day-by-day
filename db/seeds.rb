62cd721d4e25488c81a50c5018bdde89c7f525f5
Alexane Kurek <alexane.kurek7@gmail.com>
2023-06-09 14:36:17 -0400

    seed works

--- db/seeds.rb
+++ db/seeds.rb
@@ -18,16 +18,6 @@ emily.save
 pablo = User.new(first_name:"Pablo", last_name:"Hennique", email:"pablo@gmail.com", password:"111111")
 pablo.save
 
-# puts "Adding obstacles..."
-# titles = ["Fight with Mike", "Baby Sacha Crying", "Coudln't Understand a Lecture",
-#           "Career Week Was Not Helpful", "IG Post Made Me Cry"]
-# obstacles = []
-
-# titles.each do |title|
-#   obstacle = Obstacle.new(title: title)
-#   obstacles.push(obstacle)
-#   obstacle.save
-# end
 
 puts "Adding an entry..."
 users = [tom, alexane, emily, pablo]
@@ -74,33 +64,59 @@ entry_7 = Entry.create!(content: "I was unable to fall asleep last night because
                         user: users.sample,
                         obstacle: Obstacle.create!(title: "Career Week Was Not Helpful"))
 
-                        entry_7 = Entry.create!(content: "I was unable to fall asleep last night because I could not stop thinking about whether or not I will be able to find a job. I concluded that I just have to do my best and trust that things will work out in the end.",
-                          date: Date.today - 16,
-                          sentiment: "Non-positive",
-                          user: users.sample,
-                          obstacle: Obstacle.create!(title: "Career Week Was Not Helpful"))
+entry_8 = Entry.create!(content: "I was watching finding nemo and it made me cry so much. The scene with the dad really touched me. It made me think about me and my dad.",
+                        date: Date.today - 18,
+                        sentiment: "Non-positive",
+                        user: users.sample,
+                        obstacle: Obstacle.create!(title: "IG Post Made Me Cry"))
 
+entry_9 = Entry.create!(content: "Mike was being an asshole again. This time he was the one who started the discussion. It was only logical that I would tell him to stop. If he ever brings up my sister again, I’ll lose it. I’m so pissed!",
+                        date: Date.today - 19,
+                        sentiment: "Non-positive",
+                        user: users.sample,
+                        obstacle: Obstacle.find_by(title: "Fight with Mike"))
 
+entry_10 = Entry.create!(content: "I keep thinking about finding a job once I am done with the bootcamp. I sure hope it won’t be too hard given all the tech layoffs. Sophia gave me some words of encouragement. It helped a lot, but I can’t help but be doubtful at times.",
+                         date: Date.today - 21,
+                         sentiment: "Non-positive",
+                         user: users.sample,
+                         obstacle: Obstacle.find_by(title: "Career Week Was Not Helpful"))
 
-entry_content = [
-  "I was watching finding nemo and it made me cry so much. The scene with the dad really touched me. It made me think about me and my dad.",
-  "Mike was being an asshole again. This time he was the one who started the discussion. It was only logical that I would tell him to stop. If he ever brings up my sister again, I’ll lose it. I’m so pissed!",
-  "I keep thinking about finding a job once I am done with the bootcamp. I sure hope it won’t be too hard given all the tech layoffs. Sophia gave me some words of encouragement. It helped a lot, but I can’t help but be doubtful at times.",
-  "I saw this story on IG that made me shed a tear. A baby squirl thought that he was being raised by a family of cats. It made me feel so hopeful about life and nature in general. Its weird in a way, but feels good.",
-  "I woke up halfway through the night because Sacha was crying. Its been several days that he sleeps poorly. I think it is because of his mattress. Maybe Charlene is right and we should buy a new one. Regardless, it is messing up my days. I still feel tired after two cups of coffee.",
-  "Today was the first day of career week, I think there were some good tips but I don’t know if it will really help me to find a job. However, I met Sam and got me really excited about her company. I’m going to send her my CV.",
-  "I love Mondays! It’s a bit weird, but I think it’s because I finally get some break from my being a dad and I get to sip my coffee in peace.",
-  "I am happy that I found my cat. It was super scary not to see him for so long. When I found him, I recognized how much I appreciated him. I’ve had him for only 2 years but it feels like so long.",
-  "Charlene welcomed me with an amazing dinner and wine. I did not see that coming. We had a good chat and spent so much time laughing and reminiscing about our past.",
-  "Ever since I got to Canada things have been working out for the better. Sometimes I forget, but I consider myself extremely lucky to be living here. The financial and political stability do not compare at all with Mexico. And the job opportunities and quality of life is out of this world. The only thing I do miss about Mexico is the food. I wish they had good tacos here."]
+entry_11 = Entry.create!(content: "I saw this story on IG that made me shed a tear. A baby squirl thought that he was being raised by a family of cats. It made me feel so hopeful about life and nature in general. Its weird in a way, but feels good.",
+                         date: Date.today - 23,
+                         sentiment: "Non-positive",
+                         user: users.sample,
+                         obstacle: Obstacle.find_by(title: "IG Post Made Me Cry"))
 
-entry_content.each do |entry|
-  Entry.new(content: entry,
-            date: Date.today,
-            sentiment: mood.sample,
-            user: users.sample,
-            obstacle: obstacles.sample).save!
-end
+entry_12 = Entry.create!(content: "I woke up halfway through the night because Sacha was crying. Its been several days that he sleeps poorly. I think it is because of his mattress. Maybe Charlene is right and we should buy a new one. Regardless, it is messing up my days. I still feel tired after two cups of coffee.",
+                         date: Date.today - 25,
+                         sentiment: "Non-positive",
+                         user: users.sample,
+                         obstacle: Obstacle.find_by(title: "Baby Sacha Crying"))
+
+entry_13 = Entry.create!(content: "I love Mondays! It’s a bit weird, but I think it’s because I finally get some break from my being a dad and I get to sip my coffee in peace.",
+                         date: Date.today - 29,
+                         sentiment: "Positive",
+                         user: users.sample,
+                         obstacle: nil)
+
+entry_14 = Entry.create!(content: "I am happy that I found my cat. It was super scary not to see him for so long. When I found him, I recognized how much I appreciated him. I’ve had him for only 2 years but it feels like so long.",
+                         date: Date.today - 31,
+                         sentiment: "Positive",
+                         user: users.sample,
+                         obstacle: nil)
+
+entry_15 = Entry.create!(content: "Charlene welcomed me with an amazing dinner and wine. I did not see that coming. We had a good chat and spent so much time laughing and reminiscing about our past.",
+                         date: Date.today - 45,
+                         sentiment: "Positive",
+                         user: users.sample,
+                         obstacle: nil)
+
+entry_16 = Entry.create!(content: "Ever since I got to Canada things have been working out for the better. Sometimes I forget, but I consider myself extremely lucky to be living here. The financial and political stability do not compare at all with Mexico. And the job opportunities and quality of life is out of this world. The only thing I do miss about Mexico is the food. I wish they had good tacos here.",
+                         date: Date.today - 49,
+                         sentiment: "Positive",
+                         user: users.sample,
+                         obstacle: nil)
 
 
 puts "Adding recommendations"
@@ -114,14 +130,14 @@ Recommendation.create(content: "I’m sorry that you had a fight with Mike. It m
                                 This experience serves as a valuable lesson in conflict resolution, allowing you to build a stronger
                                 friendship that can endure future challenges with grace and understanding.",
                       category: "Reframing",
-                      obstacle: obstacles.first)
+                      obstacle: Obstacle.find_by(title: "Fight with Mike"))
 
 Recommendation.create(content: "It might help to practice compassion for Mike. Even though he might have wronged you,
                                 understanding how his anger made him act this way might help you process the situation.
                                 Try to visualize love and understanding for Mike. Send him good wishes and remember that
                                 he is also trying to be happy, like every human being.",
                       category: "Compassion",
-                      obstacle: obstacles.first)
+                      obstacle: Obstacle.find_by(title: "Fight with Mike"))
 
 Recommendation.create(content: "You might want to focus on the sensation of pain and anger in your body. Try closing
                                 your eyes and stay with your emotions for a few minutes. Understand how each emotion
@@ -129,4 +145,4 @@ Recommendation.create(content: "You might want to focus on the sensation of pain
                                 of anger and pain lose their intensity. When this happens, you might be better
                                 equiped to deal with Mike",
                       category: "Feel Emotions",
-                      obstacle: obstacles.first)
+                      obstacle: Obstacle.find_by(title: "Fight with Mike"))
