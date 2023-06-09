3635863362007e032a0a8c87f52d16022663735e
TomKim <ehdgus5289@gmail.com>
2023-06-09 17:48:52 -0400

    calendar not done yet

--- app/controllers/messages_controller.rb
+++
@@ -1,6 +0,0 @@
-class MessagesController < ApplicationController
-  def create
-    message = Message.create! params.require(:message).permit(:title, :content)
-    redirect_to message
-  end
-end
--- app/javascript/controllers/flatpickr_controller.js
+++ app/javascript/controllers/flatpickr_controller.js
@@ -1,6 +1,7 @@
 import { Controller } from "@hotwired/stimulus"
 import flatpickr from "flatpickr";
 
+
 // Connects to data-controller="flatpickr"
 export default class extends Controller {
   connect() {
