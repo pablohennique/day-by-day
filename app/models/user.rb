759fa462adab1196cfea8d6b36a9b21dff7709b2
TomKim <ehdgus5289@gmail.com>
2023-06-12 10:35:55 -0400

    user model conflict solved

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,12 +2,8 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
-<<<<<<< HEAD
         :recoverable, :rememberable, :validatable
-=======
-         :recoverable, :rememberable, :validatable
 
->>>>>>> master
   has_many :entries, dependent: :destroy
   has_many :gratefulnesses, dependent: :destroy
   has_many :obstacles, dependent: :destroy
