5eae2146110a8fb4096980a2a0b98227bd7a5164
Alexane Kurek <alexane.kurek7@gmail.com>
2023-06-13 17:47:51 -0400

    back to boostrap

--- app/javascript/controllers/index.js
+++ app/javascript/controllers/index.js
@@ -15,7 +15,3 @@ application.register("hello", HelloController)
 
 import ReadMoreController from "./read_more_controller"
 application.register("read-more", ReadMoreController)
-
-
-import Carousel from 'stimulus-carousel'
-application.register("carousel", Carousel)
