56375521df4659d94b4f908250461307d1494e35
Emily Brown <emi.jbrown@gmail.com>
2023-06-13 15:23:34 -0400

    added data targets to entries and obstacles headers

--- app/javascript/controllers/annotate_controller.js
+++ app/javascript/controllers/annotate_controller.js
@@ -3,19 +3,18 @@ import { annotate } from 'rough-notation'
 
 // Connects to data-controller="annotate"
 export default class extends Controller {
-  static targets = ['banner', 'cta']
+  static targets = ['banner', 'cta', 'underline', 'circle']
 
   connect() {
-    // console.log(this.bannerTarget);
-    annotate(this.bannerTarget, { type: 'highlight', color: '#FEC700' }).show();
-    annotate(this.ctaTarget, { type: 'highlight', color: '#FEC700' }).show();
+    // console.log(this.element);
+    if (this.hasBannerTarget) {
+      annotate(this.bannerTarget, { type: 'highlight', color: '#FEC700' }).show();
+    } else if (this.hasCtaTarget) {
+      annotate(this.ctaTarget, { type: 'underline', color: '#FEC700' }).show();
+    } else if (this.hasUnderlineTarget) {
+      annotate(this.underlineTarget, { type: 'underline', color: '#FEC700' }).show();
+    } else if (this.hasCircleTarget) {
+      annotate(this.circleTarget, { type: 'circle', color: '#FEC700' }).show();
+    }
   }
 }
-
-
-// export default class extends Controller {
-//   connect() {
-//     const annotation = annotate(this.element, { type: 'highlight', color: '#FEC700' });
-//     annotation.show();
-//   }
-// }
