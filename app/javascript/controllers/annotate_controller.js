418ca0b2dd825b26c080b121540f852bf9cd1222
Emily Brown <emi.jbrown@gmail.com>
2023-06-13 12:25:48 -0400

    finalized annotation on homepage

--- app/javascript/controllers/annotate_controller.js
+++ app/javascript/controllers/annotate_controller.js
@@ -3,12 +3,12 @@ import { annotate } from 'rough-notation'
 
 // Connects to data-controller="annotate"
 export default class extends Controller {
-  static targets = ['banner']
+  static targets = ['banner', 'cta']
 
   connect() {
     // console.log(this.bannerTarget);
     annotate(this.bannerTarget, { type: 'highlight', color: '#FEC700' }).show();
-
+    annotate(this.ctaTarget, { type: 'highlight', color: '#FEC700' }).show();
   }
 }
 
