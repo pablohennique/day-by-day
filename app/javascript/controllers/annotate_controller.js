b1b360961e3b1b113be1e5e676130b97fd5cdd1a
Emily Brown <emi.jbrown@gmail.com>
2023-06-13 11:49:27 -0400

    refactoring js controller to add data targets

--- app/javascript/controllers/annotate_controller.js
+++ app/javascript/controllers/annotate_controller.js
@@ -3,8 +3,19 @@ import { annotate } from 'rough-notation'
 
 // Connects to data-controller="annotate"
 export default class extends Controller {
+  static targets = ['banner']
+
   connect() {
-    const annotation = annotate(this.element, { type: 'highlight', color: '#FEC700' });
-    annotation.show();
+    // console.log(this.bannerTarget);
+    annotate(this.bannerTarget, { type: 'highlight', color: '#FEC700' }).show();
+
   }
 }
+
+
+// export default class extends Controller {
+//   connect() {
+//     const annotation = annotate(this.element, { type: 'highlight', color: '#FEC700' });
+//     annotation.show();
+//   }
+// }
