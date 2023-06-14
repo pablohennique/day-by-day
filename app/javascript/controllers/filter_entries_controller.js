cc8cb1922584c5d10530d464f60912aa6412bead
Alexane Kurek <alexane.kurek7@gmail.com>
2023-06-14 16:30:40 -0400

    search hidden and entries align

--- app/javascript/controllers/filter_entries_controller.js
+++ app/javascript/controllers/filter_entries_controller.js
@@ -2,7 +2,16 @@ import { Controller } from "@hotwired/stimulus"
 
 // Connects to data-controller="filter-entries"
 export default class extends Controller {
+  static targets = ['list', 'carousel']
+
   connect() {
     console.log("hello from filter controller");
   }
+
+  showResult(event) {
+    event.preventDefault();
+    this.listTarget.classList.remove("hidden");
+    this.carouselTarget.classList.add("hidden");
+  }
+
 }
