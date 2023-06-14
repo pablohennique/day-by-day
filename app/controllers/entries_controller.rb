eeb257b18f46c67ec840a524969d86b731c917a6
TomKim <ehdgus5289@gmail.com>
2023-06-14 15:13:46 -0400

    checklist done and calendar done

--- app/controllers/entries_controller.rb
+++ app/controllers/entries_controller.rb
@@ -93,7 +93,7 @@ class EntriesController < ApplicationController
       parameters: {
         model: "gpt-3.5-turbo",
         messages: [{ role: "user",
-                     content: "Write a gratefulness statement of 30 words or less based on
+                    content: "Write a gratefulness statement of 30 words or less based on
                               the followoing entry: #{entry}" }],
         temperature: 0.1
       }
