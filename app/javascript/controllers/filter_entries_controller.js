6cca5ca6a6b371fce465f16f69e4f70fa608dcc6
TomKim <ehdgus5289@gmail.com>
2023-06-15 14:45:52 -0400

    calendar fixed

--- app/javascript/controllers/filter_entries_controller.js
+++ app/javascript/controllers/filter_entries_controller.js
@@ -15,11 +15,12 @@ export default class extends Controller {
     this.listTarget.classList.remove("hidden");
 
     fetch(`${this.calendarTarget.action}?To=${this.inputFieldTarget.value}`, {
-      headers: { "Accept": "appliction/json"}
+      headers: { "Accept": "application/json"}
     })
     .then(response => response.json())
-    .then( data => {
-      console.log(data)
+    .then( (data) => {
+      console.log(this.listTarget)
+      this.listTarget.innerHTML = data.form
     })
   }
 }
