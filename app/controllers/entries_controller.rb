9a003c326acf2a2ff7cffce16a3df98e22f867d6
Alexane Kurek <alexane.kurek7@gmail.com>
2023-06-16 10:46:23 -0400

    add path_bar to new/edit entry and show obstacle

--- app/controllers/entries_controller.rb
+++ app/controllers/entries_controller.rb
@@ -1,6 +1,5 @@
 class EntriesController < ApplicationController
   def index
-
     @entries = Entry.where(user_id: current_user).order(date: :desc).order(id: :desc)
     # Entries - Per months
     @entries_by_months = @entries.group_by { |entry_month| entry_month.date.month }
